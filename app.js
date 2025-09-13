ad64ba24722c89fa85bfc95a1c89b503c63ddef0
KhusniddinIskandarov <kh.sh.iskandarov@gmail.com>
2025-09-13 15:34:21 +0500

    sixth exercise

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
-/* 5) prompt dan kiritilgan viloyat nomi orqali, o'sha viloyatda qancha aholi borligini alert ga chiqaring. */
+/* 6) promptdan belgilar kiritilsa, Avval meyyorda ko'rsatilgan qiymatni, kegin User ning kiritgan qiymati, agar U meyyordan oshsa, qancha oshganini, Agar meyyotdan kam bo'lsa qancha kamligi ko'rsatadigan kod yozing, Va consolga chiqaring */
 
-const viloyatNomi = prompt('Viloyat nomini kiriting!')
+let meyyor = 20
+const belgilar = prompt('belgilar kiriting!')
 
-switch(viloyatNomi) {
-    case 'Xorazm':
-        alert('Xorazmda 12 milion aholi bor')
-        break;
-    case 'Buxoro':
-        alert("Buxoroda 2,04 milion aholi bor")
-        break;
-    case 'Samarqand':
-        alert('Samarqandda 4.3 milion aholi bor')
-        break;
-    default: 
-    alert(`Siz kiritgan ${viloyatNomi}: Viloyat bazada mavjud emas`)
+if(belgilar.length > meyyor) {
+    console.log(`Siz ${meyyor}ta belgi kiritishingiz mumkin, Va Siz ${belgilar.length }ta belgi kiritdingiz, Bu meyyordan ${belgilar.length - meyyor }ta ko'p `);
+} else {
+    console.log(`Siz ${meyyor}ta belgi kiritishingiz mumkin, Va Siz ${belgilar.length }ta belgi kiritdingiz, Bu meyyordan ${meyyor - belgilar.length }ta kam `);
 }
