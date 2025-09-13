16b4adcb4e684cd744fd648fdbd49788513a3ebe
KhusniddinIskandarov <kh.sh.iskandarov@gmail.com>
2025-09-13 15:12:57 +0500

    fifth exercise

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-/* 4) array ichidagi ismlarning ohiriga  bek qo'shimchasini qo'shing va yangi array hosil qiling. Natija: shu tarzda ko'rinsin👇
+/* 5) prompt dan kiritilgan viloyat nomi orqali, o'sha viloyatda qancha aholi borligini alert ga chiqaring. */
 
-const ismlar = ['Davronbek', "Mansurbek", 'Sardorbek', 'Furqatbek'];
-*/
+const viloyatNomi = prompt('Viloyat nomini kiriting!')
 
-const ismlar = ['Davron', "Mansur", 'Sardor', 'Furqat'];
-// console.log(ismlar);
-
-const yangiIsmlar = ismlar.map((ism) => {
-    return ism.concat('bek')
-})
-console.log(yangiIsmlar);
+switch(viloyatNomi) {
+    case 'Xorazm':
+        alert('Xorazmda 12 milion aholi bor')
+        break;
+    case 'Buxoro':
+        alert("Buxoroda 2,04 milion aholi bor")
+        break;
+    case 'Samarqand':
+        alert('Samarqandda 4.3 milion aholi bor')
+        break;
+    default: 
+    alert(`Siz kiritgan ${viloyatNomi}: Viloyat bazada mavjud emas`)
+}
