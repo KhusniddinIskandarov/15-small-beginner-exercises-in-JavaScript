a5a7574166f571beaceab3a8ce50cbbe2fa99be0
KhusniddinIskandarov <kh.sh.iskandarov@gmail.com>
2025-09-13 17:36:06 +0500

    eighteenth exercise

--- app.js
+++ app.js
@@ -1,31 +1,15 @@
-/* 17) Berilgan sonni n ga ko'paytirib 1ni qo'shadigan kod yozing. Birinchi 10 ta holat qaytarilsin.
-Misol uchun:
-7 -  "8, 15, 22, 29, 36, 43, 50, 57, 64, 71"
-1 -  "2, 3 ,4 ,5, 6, 7, 8, 9, 10, 11"
-3 -  "4, 7, 10, 13, 16, 19, 22, 25, 28, 31"
-
+/* 18) Funksiyaga array kiritsak, u array ichidagi raqamlarni teskari ishorasiga aylantirib bersin.
+// [1, 2, 3, 4, 5] => [-1, -2, -3, -4, -5] 
 */
+const numbers = [1, 2, 3, 4, 5] 
+console.log(numbers);
 
-// 1-chi usul
-let number1 = 7
+let newNambers = []
 
-for (let i = 1; i <= 10; i++) {
-    console.log(number1 * i + 1);
-    
+const myFunc = (arr) => {
+    arr.forEach((number) => {
+       newNambers.push(number * -1);
+    })
 }
-
-// 2-chi usul
-let number2 = 1
-
-// for (let i = 1; i <= 10; i++) {
-//     console.log(number2 * i + 1);
-    
-// }
-
-// 3-chi usul
-let number3 = 3
-
-// for (let i = 1; i <= 10; i++) {
-//     console.log(number3 * i + 1);
-    
-// }
+myFunc(numbers)
+console.log(newNambers);
