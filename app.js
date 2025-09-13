f388ecf17369ec0548a83735f281077c171b444c
KhusniddinIskandarov <kh.sh.iskandarov@gmail.com>
2025-09-13 16:13:12 +0500

    tenth exercise

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-/* 9)  Array ichidagi  [null, 0, "",  false, undefined, NaN] qiymatlarini ajratib oladiga kod yozing.
+/* 10)  Beringan qiymat: "Uzbekistan"
+        Kutilayotgan qiymat: "natsikebzU" 
+    Hullas berilgan qiymatni tesdkari qilib beradigan kod yozing😉.
 */
-const checkData = [null,  'false', 0, 99, false, undefined,'', NaN,'0']
-let newCheckData = []
 
-checkData.forEach((data)=> {
-    if(!data) {
-        newCheckData.push(data)
-        // console.log("Kutilgan Falsy qiymatlar:",data);
-    } else {
-        // console.log("Bular esa Truety qiymatlar:",data);
-    }
+const mamlakat = "Uzbekistan"
+console.log(mamlakat);
+
+// 1-chi usul
+const result = mamlakat.split('').reverse().join("")
+console.log("Birinchi usul:" ,result);
+
+// 2-chi usul
+let natija = ''
+let boshi = mamlakat.split('').reverse()
+boshi.forEach((item) => {
+    natija += item    
 })
+console.log("Ikkinchi usul:" ,natija);
 
-console.log(newCheckData);
