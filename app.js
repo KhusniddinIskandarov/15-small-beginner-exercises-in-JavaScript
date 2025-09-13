be37592ced8f28d2dd1bb24a8f72d20e47af1850
KhusniddinIskandarov <kh.sh.iskandarov@gmail.com>
2025-09-13 17:25:49 +0500

    seventeenth exercise

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
-/* 16) Berilgan sonning "tarkibidagi" sonlar yig'indisini toping.
-        Misol uchun:
-        1 + 2 + 3 = 6
-        1 + 2 + 3 + ... + 10 = 55
-        1 + 2 + 3 + ... 7 = 28
+/* 17) Berilgan sonni n ga ko'paytirib 1ni qo'shadigan kod yozing. Birinchi 10 ta holat qaytarilsin.
+Misol uchun:
+7 -  "8, 15, 22, 29, 36, 43, 50, 57, 64, 71"
+1 -  "2, 3 ,4 ,5, 6, 7, 8, 9, 10, 11"
+3 -  "4, 7, 10, 13, 16, 19, 22, 25, 28, 31"
+
 */
 
-let number = 0
-
 // 1-chi usul
-// for (let i = 1; i <= 10; i++) {
-//    console.log(number += i)
-// }
+let number1 = 7
 
-// 2-chi usul
-for (let i = 1; i <= 7; i++) {
-    number += i
+for (let i = 1; i <= 10; i++) {
+    console.log(number1 * i + 1);
+    
 }
 
-console.log(number);
+// 2-chi usul
+let number2 = 1
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(number2 * i + 1);
+    
+// }
+
+// 3-chi usul
+let number3 = 3
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(number3 * i + 1);
+    
+// }
