2f986ed72348df32a7d821822c06952dc4a671f1
KhusniddinIskandarov <kh.sh.iskandarov@gmail.com>
2025-09-13 14:52:43 +0500

    third exercise

--- app.js
+++ app.js
@@ -1,27 +1,10 @@
-/* 2) Promptdan ism kiritilsa, ismni bosh harfi katta qolganlari kichik     harflarda chiqishi kerak bo'lgan kodni yozing! 
+/* 3) ismlar array'i ichida, promptdan so'ralayotgan ism bo'lsa, ha bunday  ism bor, agar bo'lmasa, afsus bunday ism yo'q degan yozuv consolga chiqarilishi kerak. */
 
-Kegin promptdan  yosh kiritilsa, kiritilgan yosh orqali ushbu malumotlar hisoblanib berilishi kerak:
+const ismlar = ['Husniddin', "Ag'abek", 'Sardor', 'Jaloliddin', 'Siyaush'];
+const ism = prompt("Kim kerak: ?")
 
-Yani natijada alert(`Salom ${tayorIsm}! Dasturlashga hush kelibsiz! Va Tuglilgan yil (1995), yashagan oy, hafta, kun, soat, daqiqa, soniya larni chiqazib beradigan kodni yozing`)
-*/
-
-let ism = prompt("Ism kiriting!")
-const yosh = prompt("Yoshingizni kiriting!")
-
-// Ism qismi
-let ismBoshi = ism.charAt().toUpperCase()
-let ismDavomi = ism.slice(1).toLowerCase()
-let tayorIsm = ismBoshi.concat(ismDavomi)
-
-// yosh qismi
-let yil = 2025 - yosh
-let oy = yosh * 12
-let hafta = ((yosh * 365) / 4).toFixed()
-let kun = yosh * 365
-let soat = kun * 24
-let daqiqa = soat * 60
-let soniya = daqiqa * 60
-
-alert(`Salom ${tayorIsm}! Siz ${yil}da Tug'ilgansiz, Va siz ${oy} oy, ${hafta} hafta, ${kun} kun, ${soat} soat, ${daqiqa} daqiqa, ${soniya} soniya yashagansiz 😊 !!!`)
-
-console.log(yil);
+if(ismlar.includes(ism)) {
+    console.log("Ha bunday ism ro'yhatda bor!");
+} else {
+    console.log("Afsus, Bunday ism ro'yhatda yo'q.");
+}
